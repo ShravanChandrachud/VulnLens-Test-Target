cb7d6ef6d030c5bf32e2ebeefd32e27f6b2d1f14
Sameer Saxena <sameersaxena2810@gmail.com>
2026-06-13 18:28:42 -0400

    test: full vulnerable file

--- src/vulnerable.js
+++ src/vulnerable.js
@@ -1,9 +1,16 @@
 const mysql = require('mysql');
 const API_KEY = "hardcoded-secret-123";
+const DB_PASSWORD = "supersecret123";
+const SERVER_IP = "192.168.1.100";
 
 function getUser(id) {
   const query = "SELECT * FROM users WHERE id = " + id;
   db.query(query);
 }
-// updated Sat, Jun 13, 2026  6:21:19 PM
-const SECRET = 'hardcoded-password-123';
+
+function search(username) {
+  const query = { username: username, $where: "this.password == '" + username + "'" };
+  db.collection('users').find(query);
+}
+
+const result = eval(req.body.expression);
