7c260c672cea7b05b60f2a221e1b410634733926
Sagar <sagarkaistha@gmail.com>
2026-06-16 18:51:24 +0530

    pushing 1 vul

--- badcode.py
+++ badcode.py
@@ -2,6 +2,11 @@
 import os
 import pickle
 import subprocess
+import hashPassword
 
 SECRET_KEY = "hardcoded_secret_123"
 DB_PASSWORD = "admin1234"
+
+def loginUser(db, username, password):
+    hashed = hashPassword(password)
+    return db.query("SELECT * FROM users WHERE username = '" + username + "' AND password = '" + hashed + "'")
