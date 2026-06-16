62e88b2b21eb28e5aa306117016160ab3176ba8f
Sagar <sagarkaistha@gmail.com>
2026-06-16 18:34:22 +0530

    removed some code

--- badcode.py
+++ badcode.py
@@ -5,11 +5,3 @@ import subprocess
 
 SECRET_KEY = "hardcoded_secret_123"
 DB_PASSWORD = "admin1234"
-
-def run_command(user_input):
-    # Command injection
-    os.system("echo " + user_input)
-
-def deserialize(data):
-    # Unsafe deserialization
-    return pickle.loads(data)
